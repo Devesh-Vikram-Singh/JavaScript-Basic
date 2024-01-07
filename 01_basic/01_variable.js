dd9e888f8cb5436c723d29196c13cdb114f4836c
Devesh-Vikram-Singh <122185147+Devesh-Vikram-Singh@users.noreply.github.com>
2024-01-07 19:41:58 +0000

    command config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 8940
+let accountEmail="deveshvikra@gmail.com"
+var accountPassword="12345"
+accountCity="Raebareli"
+//accountId=2;
+accountEmail="xyz.com"
+accountPassword="1234555"
+accountCity="Amethi"
+let accountState;
+console.log(accountId);
+/*
+prefer not to use to var:because of issue in block scope and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basic/datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs,not browser
+let name="Vaibhav"
+let age =18
+let isLoggedIn=false
+let state;
+
+//number => 2 to pwer 53
+//bigint
+//string=>""
+//boolean => true/false
+//null = standalone value
+// undefined=>
+//symbol=>unique
+
+//OBJECT
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
