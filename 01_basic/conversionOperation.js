110a304291a266c1e176dcac025de1efc481ad74
Devesh-Vikram-Singh <122185147+Devesh-Vikram-Singh@users.noreply.github.com>
2024-01-07 19:58:55 +0000

    conversion of datatype

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=> 33
+//"33absc"=> NaN(Not a Number) but type is Number
+//true=> 1; false=>0
+
+let isLoggedIn=1
+let isBooleanLoggedIn=Boolean(isLoggedIn);
+console.log(isBooleanLoggedIn);
+// "" =false "some value"=true
+// 1=true 0=false
+ 
+let Number=33
+let stringNumber=String(Number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
