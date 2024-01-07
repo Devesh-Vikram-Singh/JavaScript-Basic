3f245cfe7aba0cec1995cf36541786d6ea068941
Devesh-Vikram-Singh <122185147+Devesh-Vikram-Singh@users.noreply.github.com>
2024-01-07 20:14:13 +0000

    conversion and operation of JS

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -1,4 +1,4 @@
-let score="33abc"
+/*let score="33abc"
 console.log(typeof score);
 console.log(typeof (score));
 
@@ -14,8 +14,34 @@ let isBooleanLoggedIn=Boolean(isLoggedIn);
 console.log(isBooleanLoggedIn);
 // "" =false "some value"=true
 // 1=true 0=false
- 
-let Number=33
-let stringNumber=String(Number)
+
+let Numbers=33
+let stringNumber=String(Numbers)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+ //******* operation *********//
+ let value=3
+ let negValue=-value
+ //console.log(negValue);
+
+ let str1="hello"
+ let str2="Bharat"
+ let str3=str1+str2
+ /*console.log(str3);
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log("1"+2+2);
+ console.log(1+2+"2");*/
+
+ let gameCounter=100
+ ++gameCounter;
+ console.log(gameCounter);
+ gameCounter++;
+ console.log(gameCounter);
+
+
+ let gameCounter1=100;
+ gameCounter1++;
+ console.log(gameCounter1);
+ ++gameCounter1
+ console.log(gameCounter1);
