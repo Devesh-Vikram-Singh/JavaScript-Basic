5ca1c95b740cc7269193e19007bf8af1f097067a
Devesh-Vikram-Singh <122185147+Devesh-Vikram-Singh@users.noreply.github.com>
2024-01-08 20:09:48 +0000

    Dates and time

---
+++ 01_basic/dates.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+console.log(myDate);
+
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+let myCreatedDate=new Date(2023,0,23)
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+
+console.log(Math.floor(Date.now()/1000));
+
