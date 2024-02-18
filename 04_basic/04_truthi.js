83012fc8682484910093db3828638b812771d69e
Devesh-Vikram-Singh <122185147+Devesh-Vikram-Singh@users.noreply.github.com>
2024-02-18 07:03:21 +0000

    truthy ans falsey and nullish operator

--- 04_basic/04_truthi.js
+++ 04_basic/04_truthi.js
@@ -19,3 +19,12 @@ const emptyObj={}
 if(Object.keys(emptyObj).length===0){
     console.log("Object is Empty");
 }
+//Nullish Coalescing Operator(??):null undefined
+
+let val1,val2,val3;
+val1=5 ?? 10
+val2=null ?? 10
+val3=undefined ?? 15
+console.log(val1);
+console.log(val2);
+console.log(val3);
